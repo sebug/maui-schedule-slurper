ba1bb28e96a8f45733f9ad67e60d741a3f15edf1
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-01-05 11:53:29 +0100

    Getting info for devroom

--- Platforms/iOS/Services/SynchronizationService.cs
+++ Platforms/iOS/Services/SynchronizationService.cs
@@ -1,13 +1,15 @@
 using CoreFoundation;
 using Foundation;
+using HtmlAgilityPack;
 
 namespace maui_schedule_slurper.Services;
 
 public partial class SynchronizationService
 {
+    private string _baseUrl = "https://fosdem.org";
     public void StartSynchronization()
     {
-        var baseURL = new NSUrl("https://fosdem.org/2024/schedule/");
+        var baseURL = new NSUrl(_baseUrl + "/2024/schedule/");
         var basePageTask = NSUrlSession.SharedSession.CreateDataTask(baseURL, HandleMainPageResponse);
         basePageTask.Resume();
     }
@@ -30,6 +32,28 @@ public partial class SynchronizationService
 
     private void ExtractDevroomLinksAndFollow(string mainPageContent)
     {
-
+        var doc = new HtmlDocument();
+        doc.LoadHtml(mainPageContent);
+        var devroomsHeading = doc.DocumentNode.Descendants("h3")
+        .FirstOrDefault(h3 => h3.InnerText.Contains("Developer rooms", StringComparison.InvariantCultureIgnoreCase));
+        if (devroomsHeading != null)
+        {
+            var nextSibling = devroomsHeading.NextSibling;
+            while (nextSibling != null && nextSibling.Name != "div")
+            {
+                nextSibling = nextSibling.NextSibling;
+            }
+            if (nextSibling != null)
+            {
+                var listLinks = nextSibling.Descendants("li")
+                .SelectMany(li => li.Descendants("a"))
+                .Select(a => new {
+                    Name = a.InnerText.Trim(),
+                    RelativeURL = a.GetAttributeValue("href", String.Empty)
+                })
+                .Where(o => !String.IsNullOrEmpty(o.RelativeURL))
+                .ToList();
+            }
+        }
     }
 }
