49fe23c94ce51576976fd24674d620d7c9257a05
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-01-05 13:06:22 +0100

    Getting data

--- Platforms/iOS/Services/DevroomHandler.cs
+++ Platforms/iOS/Services/DevroomHandler.cs
@@ -1,13 +1,18 @@
 using Foundation;
+using HtmlAgilityPack;
 using ObjCRuntime;
 
 namespace maui_schedule_slurper.Services;
 
 public class DevroomHandler : NSUrlSessionDataDelegate
 {
-    public override void DidReceiveResponse(NSUrlSession session, NSUrlSessionDataTask dataTask, NSUrlResponse response, Action<NSUrlSessionResponseDisposition> completionHandler)
+    private readonly List<NSData> receivedData = new List<NSData>();
+    public override void DidReceiveData(NSUrlSession session, NSUrlSessionDataTask dataTask, NSData data)
     {
-        var url = dataTask?.CurrentRequest?.Url;
+        var responseUrl = dataTask.Response.Url;
+        var asString = data.ToString();
+        var doc = new HtmlDocument();
+        doc.LoadHtml(asString);
         
     }
 }
